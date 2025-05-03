20d47ea36e4770ef18fd34e8f5f340f1f3201cf5
farhan0167 <afi225@nyu.edu>
2025-05-03 16:27:18 -0400

    implement the collections index

--- database/database.go
+++ database/database.go
@@ -7,64 +7,70 @@ import (
 
 type DB struct {
 	Tables      []Table
-	DBIndex     map[string]int
-	DBNameIndex map[string]int
+	DBIndex     Collections
+	DBNameIndex Collections
 }
 
 func (db *DB) GetTables() []Table {
 	return db.Tables
 }
 func (db *DB) GetTableById(Id string) (Table, error) {
-	index, ok := db.DBIndex[Id]
-	if ok {
-		return db.Tables[index], nil
+	//index, ok := db.DBIndex[Id]
+	index, err := db.DBIndex.Search(Id)
+	if err != nil {
+		return Table{}, fmt.Errorf("table with id %s not found", Id)
 	}
-	return Table{}, fmt.Errorf("table with id %s not found", Id)
+	return db.Tables[index], nil
 }
 
 func (db *DB) GetTableByName(Name string) (Table, error) {
-	index, ok := db.DBNameIndex[Name]
-	if ok {
-		return db.Tables[index], nil
+	index, err := db.DBNameIndex.Search(Name)
+	if err != nil {
+		return Table{}, fmt.Errorf("table with name %s not found", Name)
 	}
-	return Table{}, fmt.Errorf("table with name %s not found", Name)
+	return db.Tables[index], nil
 }
 
 func (db *DB) AddTable(table Table) error {
-	if db.DBIndex == nil {
-		db.DBIndex = make(map[string]int)
+	if db.DBIndex.Index == nil {
+		db.DBIndex.Init()
 	}
-	if db.DBNameIndex == nil {
-		db.DBNameIndex = make(map[string]int)
+	if db.DBNameIndex.Index == nil {
+		db.DBNameIndex.Init()
 	}
+
 	if table.Id == "" {
 		return fmt.Errorf("Table id is empty. Please set a table id.")
 	}
-	_, ok := db.DBIndex[table.Id]
-	if ok {
-		return fmt.Errorf("Table with Id %v already exists", table.Id)
+
+	_, err := db.DBIndex.Search(table.Id)
+	if err != nil {
+		fmt.Errorf("Table with Id %v already exists", table.Id)
 	}
+
 	db.Tables = append(db.Tables, table)
-	db.DBIndex[table.Id] = len(db.Tables) - 1
-	db.DBNameIndex[table.Name] = len(db.Tables) - 1
+
+	db.DBIndex.Add(table.Id, len(db.Tables)-1)
+	db.DBNameIndex.Add(table.Name, len(db.Tables)-1)
 
 	return nil
 }
 
 func (db *DB) DeleteTable(id string) error {
-	table_index, ok := db.DBIndex[id]
-	if ok != true {
+	table_index, err := db.DBIndex.Search(id)
+	if err != nil {
 		return fmt.Errorf("No Table with id %v found", id)
 	}
 	table := db.Tables[table_index]
-	delete(db.DBIndex, id)
-	delete(db.DBNameIndex, table.Name)
+	// delete(db.DBIndex, id)
+	// delete(db.DBNameIndex, table.Name)
+
+	db.DBIndex.Delete(id)
+	db.DBNameIndex.Delete(table.Name)
 
 	db.Tables = slices.Delete(db.Tables, table_index, table_index+1)
-	for index, table := range db.Tables {
-		db.DBIndex[table.Id] = index
-		db.DBNameIndex[table.Name] = index
-	}
+	db.DBIndex.Build(db.Tables, "Id")
+	db.DBNameIndex.Build(db.Tables, "Name")
 
 	return nil
 }
