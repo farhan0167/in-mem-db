7114c23dcfcc63e77925373ae101b9cb0a080e43
farhan0167 <afi225@nyu.edu>
2025-05-03 15:48:06 -0400

    create defs for indexes

---
+++ database/indexes.go
@@ -0,0 +1,59 @@
+package database
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Index interface {
+	Add(k string, v any) error
+	Delete(k string)
+	Search(k string) (any, error)
+	Init()
+	Build(structs []any, fieldName string) error
+}
+
+type Collections struct {
+	Index map[string]int
+}
+
+func (c *Collections) Init() {
+	c.Index = make(map[string]int)
+}
+
+func (c *Collections) Add(k string, v any) error {
+	index, ok := v.(int)
+	if ok {
+		c.Index[k] = index
+		return nil
+	}
+	return fmt.Errorf("v is of type %T. int expected", v)
+}
+
+func (c *Collections) Search(k string) (any, error) {
+	index, ok := c.Index[k]
+	if ok {
+		return index, nil
+	}
+	return -1, fmt.Errorf("Key %v does not exist", k)
+}
+
+func (c *Collections) Delete(k string) {
+	delete(c.Index, k)
+}
+
+func (c *Collections) Build(structs []any, fieldName string) error {
+	iterable := reflect.ValueOf(structs)
+
+	if iterable.Kind() != reflect.Slice {
+		return fmt.Errorf("structs is of type %T. slice expected", structs)
+	}
+
+	for i := 0; i < iterable.Len(); i++ {
+		structValue := iterable.Index(i)
+		fieldValue := structValue.FieldByName(fieldName)
+		c.Add(fieldValue.String(), i)
+	}
+
+	return nil
+}
