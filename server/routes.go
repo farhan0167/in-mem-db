57d5d05c7dca561a952655a904c7900c80803e41
farhan0167 <afi225@nyu.edu>
2025-05-05 00:22:24 -0400

    add table routes

--- server/routes.go
+++ server/routes.go
@@ -1,11 +1,15 @@
 package server
 
 import (
+	"farhan0167/mem-db/database"
 	"log/slog"
 	"net/http"
 )
 
-func addRoutes(mux *http.ServeMux, logger *slog.Logger) {
+func addRoutes(mux *http.ServeMux, logger *slog.Logger, db *database.DB) {
 
-	mux.Handle("GET /hello", LoggingMiddleware(logger, http.HandlerFunc(TestRouteHandler)))
+	mux.Handle("GET /tables", LoggingMiddleware(logger, HandleGetTables(db)))
+	mux.Handle("GET /tables/id/{id}", LoggingMiddleware(logger, HandleGetTableById(db)))
+	mux.Handle("GET /tables/name/{name}", LoggingMiddleware(logger, HandleGetTableByName(db)))
+	mux.Handle("POST /tables", LoggingMiddleware(logger, HandleAddTable(db)))
 }
