a475a5dde9bfda952abb6d16994232ebdba04ecf
farhan0167 <afi225@nyu.edu>
2025-05-05 00:22:06 -0400

    write an encode/decode function for json handling

--- server/middleware.go
+++ server/middleware.go
@@ -1,17 +1,32 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
 )
 
-func TestRouteHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+func encode[T any](w http.ResponseWriter, r *http.Request, status int, v T) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		return fmt.Errorf("encode json: %w", err)
+	}
+	return nil
+}
+
+func decode[T any](r *http.Request) (T, error) {
+	var v T
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		return v, fmt.Errorf("decode json: %w", err)
+	}
+	return v, nil
 }
 
 func LoggingMiddleware(logger *slog.Logger, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.Info("Request", "method", r.Method, "path", r.URL.Path)
+		logger.Info("", "method", r.Method, "path", r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
